eadc0f1229a31dacecfe035d58ae237626145c28
Vlad <vlad@hotmail.co.nz>
2018-03-06 16:31:54 +1300

    starting

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes.js');
+
 const _ = require('lodash');
 
-
-// console.log(_.isString(true));
-// console.log(_.isString('vlad'));
-
-var filteredArray = _.uniq(["Vlad", 1, "Vlad", 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// let res = notes.add(7,7);
-// console.log('Result: ', res);
-
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `hello ${user.username} You are
-//   ${notes.age}`, (err) => {
-//   if (err) {
-//     console.log('unable to write to file');
-//   }
-// });
 
--- notes.js
+++ notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
   console.log('add note');
   return 'New note'
 };
-
-module.exports.add = (a,b) => {
-  return a + b;
-};
